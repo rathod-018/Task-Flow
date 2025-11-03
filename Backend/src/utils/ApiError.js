46650547953ea9e3a88f328593c59604c1942929
Shreyas K <shrathod6362@gmail.com>
2025-11-03 15:27:09 +0530

    add ApiError, ApiResponse, and asyncHandler utility classes

---
+++ Backend/src/utils/ApiError.js
@@ -0,0 +1,19 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        stack = "") {
+        super(message)
+        this.statusCode = statusCode
+        this.message = message
+        if (stack) {
+            this.stack = stack
+        }
+        else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+
+export { ApiError }
