a879ac0855acb06e239ebe04312c57740c5c4fa2
Shreyas K <shrathod6362@gmail.com>
2025-11-06 00:26:52 +0530

    refactor: move error handling middleware to app.js for better organization

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,33 @@
 import express from "express"
+import cookieparser from "cookie-parser"
 
 const app = express()
+
+
+
+app.use(express.json())
+app.use(express.static("public"))
+app.use(express.urlencoded())
+app.use(cookieparser())
+
+
+// error handling middleware for custum json response
+app.use((err, req, res, next) => {
+    res.status(err.code || 500).json({
+        success: false,
+        message: err.message || "Internal server error",
+        errors: err.errors || []
+    })
+})
+
+
+
+
+
+
+
+
+
 
 app.get("/", (req, res) => {
     res.send("Hello World")
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,17 +5,6 @@ import { dbConnection } from "./db/db.js"
 
 dotenv.config({ path: "../.env" })
 
-app.use(express.json())
-
-// error handling middleware for custum json response
-app.use((err, _, res, _) => {
-    res.status(err.code || 500).json({
-        success: false,
-        message: err.message || "Internal server error",
-        errors: err.errors || []
-    })
-})
-
 
 
 const port = process.env.PORT || 3000
