50fb00b4f5c22046b3313e586d86be981c342db5
Shreyas K <shrathod6362@gmail.com>
2025-11-11 01:37:51 +0530

    refactor: remove password hashing and validation methods from user schema

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -40,15 +40,6 @@ const userShema = new Schema(
         timestamps: true
     })
 
-userShema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next()
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
-
-userShema.methods.validatePassword = async function (password) {
-    return await bcrypt.compare(password, this.password)
-}
 
 
 export const User = mongoose.model("User", userShema)
