35c02c6fb3c87dc88f4a2c8a154e6e376092f8b6
Shreyas K <shrathod6362@gmail.com>
2025-11-11 00:51:28 +0530

    feat: add OTP generator utility for email verification

---
+++ Backend/src/utils/otpGenerator.js
@@ -0,0 +1,23 @@
+import { OtpModel } from "../models/otp.model.js";
+
+const otpGenerator = async (email) => {
+    if (!email) {
+        return null
+    }
+
+    let otp = ""
+    for (let i = 0; i < 5; i++) {
+        otp += Math.floor(Math.random() * 10)
+    }
+
+    await OtpModel.create({
+        email,
+        otp,
+        expiredAt: Date.now() + 5 * 60 * 1000
+    })
+
+    return otp;
+
+}
+
+export { otpGenerator }
