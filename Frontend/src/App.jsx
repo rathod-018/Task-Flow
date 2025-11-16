26c9afc5273fb7659861de3256a138c06eaf40f2
Shreyas K <shrathod6362@gmail.com>
2025-11-17 00:46:48 +0530

    update routing and replace components in App, add new SVG assets

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,23 @@
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import {
-  Home,
-  SignUp,
-  VerifyOtp,
-  CreateProject,
-  LogIn,
-  Welcome,
-} from "./pages/index";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import LogIn from "./pages/auth/LogIn";
+import SignUp from "./pages/auth/Signup";
+import VerifyOtp from "./pages/auth/VerifyOtp";
+import CreateBoard from "./pages/CreateBoard";
 import Protected from "./components/Protected";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Welcome />} />
+      <Route path="/" element={<Home />} />
       <Route
-        path="/home"
+        path="/dashboard"
         element={
           <Protected>
-            <Home />
+            <Dashboard>
+              
+            </Dashboard>
           </Protected>
         }
       />
@@ -26,10 +25,10 @@ function App() {
       <Route path="/signup" element={<SignUp />} />
       <Route path="/verify-otp" element={<VerifyOtp />} />
       <Route
-        path="/create-project"
+        path="/board"
         element={
           <Protected>
-            <CreateProject />
+            <CreateBoard />
           </Protected>
         }
       />
