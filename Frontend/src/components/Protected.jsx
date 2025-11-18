b7ac6a736a8f4021d251e0320ca649d3af59ee9f
Shreyas K <shrathod6362@gmail.com>
2025-11-18 22:23:42 +0530

    refactor protected component

--- Frontend/src/components/Protected.jsx
+++ Frontend/src/components/Protected.jsx
@@ -1,20 +1,14 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import { useUserContext } from "../context/UserContext";
 
-function Protected({ children }) {
-  const navigate = useNavigate();
+function Protected() {
   const { user, loading } = useUserContext();
-
-  useEffect(() => {
-    if (!loading && !user) {
-      navigate("/login");
-    }
-  }, [loading, user]);
 
   if (loading) return <div>Loading...</div>;
 
-  return <>{children}</>;
+  if (!user) return <Navigate to="/login" replace />;
+
+  return <Outlet />;
 }
 
 export default Protected;
