42600476c0fe21fee7dfc3ccca48c457e021eba2
Shreyas K <shrathod6362@gmail.com>
2025-11-21 00:03:10 +0530

    add constom hooks for to get created and updated boards and update user page history

---
+++ Frontend/src/hooks/useFetchBoard.js
@@ -0,0 +1,31 @@
+import api from "../api/axios";
+import { useEffect, useState } from "react";
+import { useUserContext } from "../context/UserContext";
+
+export const useFetchBoard = (url) => {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    const { user } = useUserContext()
+
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const { data } = await api.get(url)
+                console.log(data)
+                setData(data?.data)
+
+            } catch (error) {
+                setError(error.response?.data?.message || "something went wrong")
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchData()
+    }, [user])
+
+    return { data, loading, error }
+
+}
---
+++ Frontend/src/hooks/usePageHisrory.js
@@ -0,0 +1,19 @@
+import api from "../api/axios";
+import { useUserContext } from "../context/UserContext";
+
+export const usePageHistory = () => {
+    const { getUser } = useUserContext();
+
+    const updateLastOpened = async (boardId = null, projectId = null) => {
+        try {
+            const { data } = await api.patch("/user/page-history", { boardId, projectId })
+            if (data.statusCode === 200) {
+                getUser()
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return { updateLastOpened }
+}
