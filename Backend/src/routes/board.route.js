c02f6927bc5d2ffc294f8d3d409cbe6fc27a9bbb
Shreyas K <shrathod6362@gmail.com>
2025-11-21 00:43:44 +0530

    add route for board

--- Backend/src/routes/board.route.js
+++ Backend/src/routes/board.route.js
@@ -1,14 +1,13 @@
 import { Router } from "express";
 import { verifyUser } from "../middleware/userAuth.middleware.js"
-import { createBoard, getCreatedBoards } from "../controllers/board.controller.js";
+import { createBoard, getBoardById, getCreatedBoards } from "../controllers/board.controller.js";
 
 const router = Router()
 
 router.use(verifyUser)
-
 router.route("/create").post(createBoard)
-
 router.route("/created").get(getCreatedBoards)
+router.route("/:boardId").get(getBoardById)
 
 
 
