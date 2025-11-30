2861195fb0ab11f76e0777dab86692975b1e2493
Shreyas K <shrathod6362@gmail.com>
2025-12-01 01:38:13 +0530

    add routes and controller for getting and updateing task

--- Backend/src/controllers/task.controller.js
+++ Backend/src/controllers/task.controller.js
@@ -23,9 +23,9 @@ export const createTask = asyncHandler(async (req, res) => {
     if (!project) {
         throw new ApiError(400, "Invalid projectId")
     }
-
+    let assignee;
     if (assigneeId) {
-        const assignee = await User.findById(assigneeId)
+        assignee = await User.findById(assigneeId)
         if (!assignee) {
             throw new ApiError(400, "Invalid assigneeId")
         }
@@ -37,11 +37,55 @@ export const createTask = asyncHandler(async (req, res) => {
             description,
             dueDate: new Date(date) || null,
             projectId: project?._id,
-            assigneeId
+            assigneeId: assignee?._id || null
         })
 
     res.status(201).json(
         new ApiResponse(201, task, "Task Created successfully")
+    )
+
+})
+
+
+export const getAllTask = asyncHandler(async (req, res) => {
+    const { projectId } = req.params
+
+    if (!projectId || !isValidObjectId(projectId)) {
+        throw new ApiError(400, "Invalid ProjectId")
+    }
+
+    const project = await Project.findById(projectId)
+    if (!project) {
+        throw new ApiError(400, "Invalid ProjectId")
+    }
+
+    const tasks = await Task.find({ projectId: project?._id })
+
+    res.status(200).json(
+        new ApiResponse(200, tasks, "All task fetched successfully")
+    )
+})
+
+
+export const updateTaskStatus = asyncHandler(async (req, res) => {
+    const { taskId, status } = req.body
+
+    if (!status) {
+        throw new ApiError(400, "Status is required")
+    }
+
+    if (!taskId || !isValidObjectId(taskId)) {
+        throw new ApiError(400, "Invalid taskId")
+    }
+
+    const task = await Task.findByIdAndUpdate(taskId, { status }, { new: true })
+
+    if (!task) {
+        throw new ApiError(400, "Invalid taskId")
+    }
+
+    res.status(200).json(
+        new ApiResponse(200, task, "Task status updated successfully")
     )
 
 })
--- Backend/src/routes/task.route.js
+++ Backend/src/routes/task.route.js
@@ -1,12 +1,14 @@
 import Router from "express"
 import { verifyUser } from "../middleware/userAuth.middleware.js"
-import { createTask } from "../controllers/task.controller.js"
+import { createTask, getAllTask, updateTaskStatus } from "../controllers/task.controller.js"
 
 const router = Router()
 
 router.use(verifyUser)
 
 router.route("/create").post(createTask)
+router.route("/all/:projectId").get(getAllTask)
+router.route("/status").patch(updateTaskStatus)
 
 
 
