7ba150d740cd9e3679e1a7ad1c47fb6e50ffa409
Shreyas K <shrathod6362@gmail.com>
2025-12-01 01:34:39 +0530

    send relevent data each column

--- Frontend/src/components/workFlow/WorkFlow.jsx
+++ Frontend/src/components/workFlow/WorkFlow.jsx
@@ -1,16 +1,26 @@
-import React from "react";
-import { useOutletContext } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import WorkFlowCard from "./WorkFlowCard";
+import { useTaskContext } from "../../context/TaskContext";
 
 function WorkFlow() {
-  const { project } = useOutletContext();
-  // console.log(project);
+  const [tasks, setTasks] = useState([]);
+  const { taskData} = useTaskContext();
+
+  useEffect(() => {
+    setTasks(taskData);
+  }, [taskData]);
+
+  const todo = tasks.filter((t) => t.status === "todo");
+  const inProgress = tasks.filter((t) => t.status === "in_progress");
+  const done = tasks.filter((t) => t.status === "done");
+
+
 
   return (
     <div className="flex justify-evenly mt-10">
-      <WorkFlowCard type="To do" />
-      <WorkFlowCard type="In progress" />
-      <WorkFlowCard type="Done" />
+      <WorkFlowCard type="To do" tasks={todo} />
+      <WorkFlowCard type="In progress" tasks={inProgress} />
+      <WorkFlowCard type="Done" tasks={done} />
     </div>
   );
 }
